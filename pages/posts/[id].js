dd77656db754737ba7cb3dc966d4abf269b48a68
osuzuki <osuzuki@plegineer.com>
2020-08-24 12:51:18 +0900

    Css format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyle from '../../styles/utils.module.css'
 
 export default function Post( { postData }) {
   return (
@@ -9,13 +10,15 @@ export default function Post( { postData }) {
       <Head> 
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyle.headingXl}>
+          {postData.title}
+        </h1>
+        <div className={utilStyle.lightText}>
+          <Date dateString={postData.date} />  
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>)
 }
 
